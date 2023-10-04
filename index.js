a119bd008db3b594cdd8b05cc2db390f86e764fb
ayrh90-fZNzxW <ayrh90@icloud.com>
2023-10-03 17:40:43 -0700

    completed

--- index.js
+++ index.js
@@ -1,13 +1,115 @@
-// TODO: Include packages needed for this application
+// import librabries 
+const inquirer = require('inquirer');
+const generateMarkdown = require('./utils/generateMarkdown.js');
+const fs = require("fs");
 
-// TODO: Create an array of questions for user input
-const questions = [];
+// Questions to be asked by user
+let collectedAnswers = [];
+const questions = [
 
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+    {
+        type: 'input',
+        name: 'title',
+        message: "1.what is the title of your project?",
+    },
+    {
+        type: 'input',
+        name: 'description',
+        message: "2.Please Enter Description Information:",
+    },
+    {
+        type: 'input',
+        name: 'installation',
+        message: "3.Please Enter Installation Information:",
+    },
+    {
+        type: 'input',
+        name: 'usage',
+        message: "4.Please Enter Usager Information:",
+    },
+    {
+        type: 'input',
+        name: 'usageImg',
+        message: "5.Please Enter Screenshot Relative Path:",
+    },
+    {
+        type: 'list',
+        name: 'license',
+        message: "6.What Type of License: ",
+        choices: ["MIT License", "Open BSD", "Mozilla Public License 2.0","None"],
+    },
+    {
+        type: 'input',
+        name: 'features',
+        message: "7.Please Enter Project features:",
+    },
+    {
+        type: 'input',
+        name: 'contribute',
+        message: "8.Please Enter Contributor Information:",
+    },
+    {
+        //if true, function credits will start 
+        type: 'confirm',
+        name: 'addCredits',
+        message: "9.Would You Like to Add Credits?",
+        default: true,
+    },
+];
+// function is triggered only if user confirms adding credits 
+function credit(questionAnswers){
+    const credits = [
 
-// TODO: Create a function to initialize app
-function init() {}
+        {
+            type: 'input',
+            name: 'name',
+            message: "Enter a Name:",
+        },
+        {
+            type: 'input',
+            name: 'url',
+            message: "Enter Url:",
+        },
+        {
+            type: 'confirm',
+            name: 'askAgain',
+            message: "Would You Like to Add More Credits?",
+            default: true,
+        },
+    ];
+    inquirer.prompt(credits).then((creditAnswers) => {
+        collectedAnswers.push(creditAnswers);
+        if (creditAnswers.askAgain) {
+            credit(questionAnswers);
+        }else{
+            writeToFile('README.md', questionAnswers, collectedAnswers);
+        }
+      });
+};
+
+
+function writeToFile(fileName, questionAnswers, creditAnswers) {
+    const generatedREADME = generateMarkdown.generateMarkdown(questionAnswers,creditAnswers);
+    fs.writeFile(fileName, generatedREADME, (error) => {
+      error ? console.log(error) : console.log("Successfully created README.md")
+    })
+  }
+
+// First funtion to be triggerd 
+function init() {
+    inquirer.prompt(questions).then(questionAnswers => {
+        //if question 9 true, call credit function 
+        if(questionAnswers.addCredits){
+            credit(questionAnswers);
+        } else {
+            // if question 9 false - call write file function, 
+            let creditAnswers = generateCredits([]);  // No credits ***
+            writeToFile('README.md', questionAnswers, creditAnswers);
+        }
+    });
+}
 
 // Function call to initialize app
 init();
+
+
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,20 +1,97 @@
-// TODO: Create a function that returns a license badge based on which license is passed in
-// If there is no license, return an empty string
-function renderLicenseBadge(license) {}
 
-// TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseBadge(license) {
+  if(license  === 'None') {
+    return '';
+  } else {
+    return `* [${license}]`;
+  }
+}
 
-// TODO: Create a function that returns the license section of README
+
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseLink(license) {
+  const myVariable = license;
+ console.log('fuction link license:',license)
+switch (myVariable) {
+  case 'MIT License':
+     return '(https://choosealicense.com/licenses/mit/)';
+  case 'Open BSD':
+      return '(https://choosealicense.com/licenses/bsd-2-clause/)';
+  case 'Mozilla Public License 2.0':
+    return '(https://choosealicense.com/licenses/mpl-2.0/)';
+  default:
+    return ' '; 
+}
+}
 
-// TODO: Create a function to generate markdown for README
-function generateMarkdown(data) {
-  return `# ${data.title}
+
+// If there is no license, return an empty string
+function renderLicenseSection(license) {
+  if(license  === 'None') {
+    return '';
+  }
+  
+  return '## License';
+}
+
+function generateCredits(creditAnswers) {
+  let stringCredits = [];
+  if(!Array.isArray(creditAnswers) || creditAnswers.length === 0) {
+    return 'No Credits';
+  } else {
+    for(let i = 0; i < creditAnswers.length; i++){
+      stringCredits.push(` * [${creditAnswers[i].name}](${creditAnswers[i].url})`);
+    }
+    return stringCredits.join('\n');
+  }
+}
+
+function generateMarkdown(questionAnswers, creditAnswers) {
+  let credit = creditAnswers;
+  let answer = questionAnswers;
+  return `
+
+# ${answer.title}
+
+## Description
+${answer.description}
+
+## Table of Contents (Optional)
+- [Installation](#installation)
+- [Usage](#usage)
+- [Credits](#credits)
+- [License](#license)
+- [Features](#Features)
+- [Contribute](#Contribute)
+- [License](#license)
+- [Test](#Test)
+
+## Installation
+${answer.installation}
+## Usage
+
+${answer.usage}
+![img](./${answer.usageImg})
+
+## Credits
+${generateCredits(credit)}
+
+${renderLicenseSection(answer.license)}
+${renderLicenseBadge(answer.license)}${renderLicenseLink(answer.license)} 
+
+## Features
+${answer.features}
+
+## Contribute
+${answer.contribute}
+
+## Tests
+${answer.test}
 
 `;
 }
 
-module.exports = generateMarkdown;
+module.exports = {
+  generateMarkdown, 
+}
